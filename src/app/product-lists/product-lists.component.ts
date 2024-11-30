626c62c458ce0b58e7a59be558007f4b6b95fda4
ajeetrajbhar2505 <ajeetrajbhar2504@gmail.com>
2024-11-30 23:43:04 +0530

    Integrated search functionality

--- src/app/product-lists/product-lists.component.ts
+++ src/app/product-lists/product-lists.component.ts
@@ -6,9 +6,18 @@ import { ActivatedRoute, Router } from '@angular/router';
   templateUrl: './product-lists.component.html',
   styleUrls: ['./product-lists.component.scss'],
 })
-export class ProductListsComponent implements OnInit {
+export class ProductListsComponent {
   @Input() showheader: boolean = true
   @Input() search: boolean = false
+  searchQuery: string = '';
+  products: any[] = [
+    { productName: 'Blue Shirt', folder : 'shirt-1', productAmount: '23', totalProductsSold: 1323 },
+    { productName: 'Pink Shirt', folder : 'shirt-2', productAmount: '22', totalProductsSold: 423 },
+    { productName: 'Colour Combo Shirt', folder : 'shirt-3', productAmount: '23', totalProductsSold: 2323 },
+    { productName: 'Black Shirt', folder : 'shirt-4', productAmount: '40', totalProductsSold: 8223 },
+  ]
+  filteredProducts: any[] = [];
+
   constructor(public router: Router, private ActivatedRoute: ActivatedRoute) {
     this.ActivatedRoute.queryParams.subscribe((data: any) => {
       this.search = data.enableSearchBar || false
@@ -27,6 +36,13 @@ export class ProductListsComponent implements OnInit {
   navigateToFilter() {
     this.router.navigate(['/filter'])
   }
-  ngOnInit() { }
+
+  onSearch(query: string): void {
+    this.filteredProducts = query
+      ? this.products.filter(product =>
+        product.productName.toLowerCase().includes(query.toLowerCase())
+      )
+      : [];
+  }
 
 }
