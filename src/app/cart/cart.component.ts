8804e61e2c1971a72cd72a417eb089b35662b14d
ajeetrajbhar2505 <ajeetrajbhar2504@gmail.com>
2024-12-04 00:24:47 +0530

    minorupdate related images

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -8,6 +8,13 @@ import { Router } from '@angular/router';
 })
 export class CartComponent  implements OnInit {
 
+  products: any[] = [
+    { productName: 'Blue Shirt', folder : 'shirt-1', productAmount: '23', totalProductsSold: 1323 },
+    { productName: 'Pink Shirt', folder : 'shirt-2', productAmount: '22', totalProductsSold: 423 },
+    { productName: 'Colour Combo Shirt', folder : 'shirt-3', productAmount: '23', totalProductsSold: 2323 },
+    { productName: 'Black Shirt', folder : 'shirt-4', productAmount: '40', totalProductsSold: 8223 },
+  ]
+  
   constructor(private router:Router) { }
 
   ngOnInit() {}
