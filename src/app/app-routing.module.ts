c6f32a5afa92bb35b5ad73fc375d4152603d2285
ajeetrajbhar2505 <ajeetrajbhar2504@gmail.com>
2024-12-07 13:38:27 +0530

    integrated location components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -44,6 +44,10 @@ const routes: Routes = [
     path: 'checkout',
     loadChildren: () => import('./checkout/checkout.module').then(m => m.CheckoutModule)
   },
+  {
+    path: 'location',
+    loadChildren: () => import('./location/location.module').then(m => m.LocationModule)
+  },
 ];
 @NgModule({
   imports: [
