b1924afa5283b09aa99a0c1a3eec0c153fef806b
ajeetrajbhar2505 <ajeetrajbhar2504@gmail.com>
2024-12-24 16:41:45 +0530

    added loader

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,16 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  showLoader = true;
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    // Listen for navigation events
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        // Hide loader after navigation completes
+        this.showLoader = false;
+      }
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoaderComponent } from './loader/loader.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent,LoaderComponent],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  exports : [LoaderComponent],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
