382165d660a92f822b6f0a888856e5fb10461b52
ajeetrajbhar2505 <ajeetrajbhar2504@gmail.com>
2025-01-18 00:11:33 +0530

    added animation to first stepper

--- src/app/track-order/track-order.component.ts
+++ src/app/track-order/track-order.component.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
   templateUrl: './track-order.component.html',
   styleUrls: ['./track-order.component.scss'],
 })
-export class TrackOrderComponent  implements OnInit {
+export class TrackOrderComponent implements OnInit {
   products: any[] = [
     { productName: 'Double Cuff Blue Shirt', folder: 'shirt-1', productAmount: '23', totalProductsSold: 1323 },
   ]
@@ -31,7 +31,7 @@ export class TrackOrderComponent  implements OnInit {
       text:
         "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugiat pariatur minima nemo? Facilis veniam reprehenderit quaerat aspernatur, quis voluptas voluptate."
     },
-      {
+    {
       complete: true,
       title: "Verified Payments - Dec 15",
       text:
@@ -41,10 +41,12 @@ export class TrackOrderComponent  implements OnInit {
 
   tickIcon = `<svg viewBox="0 0 512 512" width="100" title="check">
         <path d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z" />`;
-        
-  constructor(private router:Router) { }
 
-  ngOnInit() {}
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+
+  }
 
 
   goBack() {
